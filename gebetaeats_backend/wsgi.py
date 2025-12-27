845608a8636d5b26ee1d09cf0efb32589ad4e14e
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-12-27 23:51:54 +0300

    Delete gebetaeats_backend directory

--- gebetaeats_backend/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""WSGI config for gebetaeats_backend project."""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gebetaeats_backend.settings")
-
-application = get_wsgi_application()
