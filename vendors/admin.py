4d80727f65f31bf3c000568b87fa5d009e6aec5b
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-12-27 23:53:01 +0300

    Delete vendors directory

--- vendors/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Vendor
-
-
-@admin.register(Vendor)
-class VendorAdmin(admin.ModelAdmin):
-    list_display = ("id", "restaurant_name", "is_open", "rating")
-    search_fields = ("restaurant_name", "location")
--- vendors/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import VendorDetailView, VendorListView, VendorMeView
-
-urlpatterns = [
-    path("", VendorListView.as_view(), name="vendor-list"),
-    path("me/", VendorMeView.as_view(), name="vendor-me"),
-    path("<int:pk>/", VendorDetailView.as_view(), name="vendor-detail"),
-]
